fd9467ed13a8da5b2256f7ae91a5237fc181ccd2
LuisAntonioQuezadaAristega <107262704+LuisAntonioQuezadaAristega@users.noreply.github.com>
2022-10-24 23:20:16 -0500

    Se asigno color de fondo a cada botón

--- Observer/src/main/java/ventana/tarea1_aj/App.java
+++ Observer/src/main/java/ventana/tarea1_aj/App.java
@@ -20,20 +20,28 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Pane esctructura = new Pane();
-        Button btn1 = new Button("Amarillo");
+        Button btn1 = new Button("Yellow");
         btn1.setLayoutX(20);
         btn1.setLayoutY(30);
-        Button btn2 = new Button("Azul");
+        btn1.setOnAction(e->{cambiarColor(esctructura, btn1.getText());});
+        Button btn2 = new Button("Blue");
         btn2.setLayoutX(120);
         btn2.setLayoutY(30);
-        Button btn3 = new Button("Rojo");
+        btn2.setOnAction(e->{cambiarColor(esctructura, btn2.getText());});
+        Button btn3 = new Button("Red");
         btn3.setLayoutX(220);
         btn3.setLayoutY(30);
+        btn3.setOnAction(e->{cambiarColor(esctructura, btn3.getText());});
         esctructura.getChildren().addAll(btn1, btn2, btn3);
         scene = new Scene(esctructura, 300, 150);
         stage.setScene(scene);
         stage.show();
         stage.setTitle("Botones");
+        
+    }
+    
+    public void cambiarColor(Pane esctructura, String color) {
+    	esctructura.setStyle("-fx-background-color:" + color);
     }
 
     public static void main(String[] args) {
